977d50d2cfcd6f115e9dbb19d5345141d3dd9ecd
anserlt <caohaoran@unittec.com>
2022-05-23 18:42:15 +0800

    2022.5.23

--- src/main/java/com/anserlt/common/util/http/HttpUtil.java
+++ src/main/java/com/anserlt/common/util/http/HttpUtil.java
@@ -1,5 +1,6 @@
 package com.anserlt.common.util.http;
 
+import com.alibaba.fastjson.JSONObject;
 import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
@@ -8,9 +9,16 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.HttpClients;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.util.ObjectUtils;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Anserlt
@@ -37,10 +45,9 @@ public class HttpUtil {
      * @param host ip地址
      * @param port 端口
      * @param url 请求的路径
-     * @param requestType 请求类型
      * @return 响应
      */
-    public HttpResponse sendHttpRequest(String host, Integer port, String url, String requestType) {
+    public HttpResponse sendHttpGetRequestByHttpClient(String host, Integer port, String url) {
         String requestUrl = host + ":" + port + "/" + url;
         HttpClient httpClient = createDigestHttpClient(host, port, "admin", "hik12345+");
 
@@ -52,6 +59,58 @@ public class HttpUtil {
             e.printStackTrace();
         }
         return response;
+    }
+
+    /**
+     * 构造需要用户名、密码Digest认证的HTTP请求客户端
+     * @param host ip地址
+     * @param port 端口
+     * @param username 用户名
+     * @param password 密码
+     * @return RestTemplate客户端
+     */
+    public RestTemplate createDigestRestTemplate(String host, Integer port, String username, String password){
+        HttpClient httpClient = createDigestHttpClient(host, port, username, password);
+        RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory(httpClient));
+        //支持中文编码
+        restTemplate.getMessageConverters().set(1,
+                new StringHttpMessageConverter(StandardCharsets.UTF_8));
+
+        return restTemplate;
+    }
+
+    /**
+     *
+     * @param host ip
+     * @param port 端口
+     * @param url 请求地址
+     * @return ResponseEntity<T>
+     */
+    public ResponseEntity<String> sendHttpGetRequestByRestTemplate(String host, Integer port, String url) {
+        RestTemplate restTemplate = createDigestRestTemplate(host, port, "admin", "hik12345+");
+
+        Map<String, String> params = new HashMap<>();
+        params.put("host", host);
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class, params);
+
+        return responseEntity;
+    }
+
+    /**
+     *
+     * @param host ip
+     * @param port 端口
+     * @param url 请求地址
+     * @return ResponseEntity<T>
+     */
+    public ResponseEntity<String> sendHttpPostRequestByRestTemplate(String host, Integer port, String url) {
+        RestTemplate restTemplate = createDigestRestTemplate(host, port, "admin", "hik12345+");
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("host", host);
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(url, jsonObject, String.class);
+
+        return responseEntity;
     }
 
 }
